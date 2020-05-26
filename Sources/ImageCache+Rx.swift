1fc02ea307a053c10b67975b75bad91641c3360d
MohammedTao <984259107@qq.com>
2020-05-26 09:18:16 +0800

    Create ImageCache+Rx.swift

---
+++ Sources/ImageCache+Rx.swift
@@ -0,0 +1,26 @@
+extension Reactive where Base: ImageCache {
+    public func retrieveCacheSize() -> Observable<Int> {
+        return Single.create { single in
+            self.base.calculateDiskStorageSize { result in
+                do {
+                    single(.success(Int(try result.get())))
+                } catch {
+                    single(.error(error))
+                }
+            }
+            return Disposables.create {}
+        }.asObservable()
+    }
+
+    public func clearCache() -> Observable<Void> {
+        return Single.create { single in
+            self.base.clearMemoryCache()
+            self.base.clearDiskCache(completion: {
+                single(.success(()))
+            })
+            return Disposables.create {}
+        }.asObservable()
+    }
+}
+
+extension ImageCache: ReactiveCompatible {}
